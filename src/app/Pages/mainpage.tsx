416c43d7a6458af74d0e3b08b344373d29c23158
YASH SINHA <yashsinhaoffcial@gmail.com>
2024-09-25 17:33:09 +0530

    main page addition

--- src/app/Pages/mainpage.tsx
+++ src/app/Pages/mainpage.tsx
@@ -1,27 +1,51 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Box from "../components/audiofilebox";
+import DroppableArea from "../components/DroppableArea";
+
+const songList = [
+    { id: 1, name: "Song 1" },
+    { id: 2, name: "Song 2" },
+    { id: 3, name: "Song 3" },
+    { id: 4, name: "Song 4" },
+    { id: 5, name: "Song 5" },
+    { id: 6, name: "Song 6" },
+];
 
 const MainPage = () => {
-    return (
-        <div className="flex flex-col min-h-screen bg-gray-900">
-            
-            <div className="flex flex-row h-[5vh] bg-gray-900"></div>
-            <div className="flex flex-row h-[60vh] bg-gray-900">
-              
-                 <div className="flex flex-col w-[15%] bg-blue-500">
-                 <div className="flex flex-col h-[10%] bg-blue-500">
-                  </div>
+    const [droppedSongs, setDroppedSongs] = useState<string[]>([]);
 
-                    <div className="flex flex-col h-[90%] bg-blue-500 overflow-y-scroll">
+    const handleDrop = (songName: string) => {
+        setDroppedSongs((prev) => [...prev, songName]); 
+    };
+
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div className="flex flex-col min-h-screen bg-gray-900">
+                
+                <div className="flex flex-row h-[5vh] bg-gray-900"></div>
+
+            
+                <div className="flex flex-row h-[60vh] bg-gray-900">
+                  
+                    <div className="flex flex-col w-[15%] bg-blue-500">
+                    <div className="flex flex-row h-[10%] bg-gray-400"></div>
+                        <div className="flex flex-col h-[90%] bg-blue-500 overflow-y-scroll">
+                            <div className="p-2">
+                                {songList.map((song) => (
+                                    <Box key={song.id} name={song.name} />
+                                ))}
+                            </div>
+                        </div>
                     </div>
 
-                  </div>
-               
-
-                  <div className="flex flex-col w-[85%] bg-blue-300 overflow-y-scroll">
-                  </div>
+                    <DroppableArea droppedSongs={droppedSongs} onDrop={handleDrop} />
+                </div>
             </div>
-        </div>
+        </DndProvider>
     );
-}
+};
 
 export default MainPage;
--- src/app/components/audiofilebox.tsx
+++ src/app/components/audiofilebox.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
 interface BoxProps {
-    name: string;  
+    name: string;
 }
 
 const Box: React.FC<BoxProps> = ({ name }) => {
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "BOX",
+        item: { name },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    }));
+
     return (
-        <div className="w-full p-4 bg-gray-700 text-white rounded-lg my-2">
+        <div
+            ref={drag}
+            className={`w-full p-4 bg-gray-700 text-white rounded-lg my-2 cursor-move ${
+                isDragging ? "opacity-50" : "opacity-100"
+            }`}
+        >
             {name}
         </div>
     );
